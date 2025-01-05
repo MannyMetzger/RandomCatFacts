1f89923e5f5e5befe345b8d60ef8f54233a104f2
Manuel Metzger <manuelmetzger01@gmail.com>
2025-01-04 23:02:06 -0500

    Initial commit for random cat facts website

---
+++ api/catfacts.js
@@ -0,0 +1,25 @@
+//import things
+import express from "express";
+import axios from "axios";
+
+const app = express();
+app.set("view engine". "ejs");
+app.use(express.static("public"));
+
+//route to render the main page
+app.get("/", (req, res) => {
+    res.render("index", {fact:null });
+});
+
+//route to fetch a random cat fact
+app.get("/get-fact", async (req, res) => {
+    try {
+        const response = await axios.get("https://catfact.ninja/fact");
+        res.render("index", { fact: response.data.fact });
+    } catch (error) {
+        res.render("index", { fact: "Error fetching cat fact." });
+    }
+});
+
+//export for vercel
+export default app;
